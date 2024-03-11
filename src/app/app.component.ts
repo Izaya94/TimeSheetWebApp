53daf0f6b9c97d848473620baaa1dd943bc19e79
Koral Krishna <krishnasoftech008@hotmail.com>
2024-03-11 13:42:19 +0530

    Got data from .NET core 8 API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule, HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
@@ -7,9 +10,22 @@ import { PrimeNGConfig } from 'primeng/api';
 })
 export class AppComponent implements OnInit {
 
+    title = 'TestTime';
+    httpClient = inject(HttpClient);
+    products:any = [];
+    fetchProducts(): void {
+        this.httpClient.get("http://localhost:5055/Products").subscribe((data:any)=>{
+            this.products = data;
+            console.log(this.products);            
+        });
+        
+    }
+
     constructor(private primengConfig: PrimeNGConfig) { }
 
-    ngOnInit() {
+    
+    ngOnInit():void{
         this.primengConfig.ripple = true;
+        this.fetchProducts();
     }
 }
