06870185035a131a83a5272201994dbc3284f16c
Izaya94 <pujarakoral@gmail.com>
2024-05-06 21:23:59 +0530

    06-05-24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatButtonModule, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, MatButtonModule, NavbarComponent, ToastModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,18 +6,25 @@ import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { ButtonModule } from 'primeng/button';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatInputModule, MatIconModule, ReactiveFormsModule, RouterLink, MatSnackBarModule, ButtonModule],
+  imports: [MatInputModule, MatIconModule, ReactiveFormsModule, RouterLink, MatSnackBarModule, ButtonModule, ToastModule,],
+  providers:[MessageService],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit {
 
-  authService = inject(AuthService);
+  // authService = inject(AuthService);
   matSnackBar = inject(MatSnackBar);
+
+  constructor(private authService: AuthService, private messageService: MessageService) {}
+
   router = inject(Router);
   hide = true;
   loginForm!: FormGroup;
@@ -27,20 +34,33 @@ export class LoginComponent implements OnInit {
     // this.authService.login(this.loginForm.value).subscribe((response) => {console.log(response);}); 
     this.authService.login(this.loginForm.value).subscribe({
       next:(response) => {
-        this.matSnackBar.open(response.message, 'Close',{
-          duration:5000,
-          horizontalPosition: 'center',
-        });
+        this.messageService.add({severity:'success', summary: 'Success', detail: response.message});
         this.router.navigate(['/']);
       },
       error: (error) => {
-        this.matSnackBar.open(error.error.message, 'Close', {
-          duration:5000,
-          horizontalPosition: 'center',
-        });
+        this.messageService.add({severity:'error', summary: 'Error', detail: error.error.message});
       },
     });
   }
+
+  // login(){
+  //   // this.authService.login(this.loginForm.value).subscribe((response) => {console.log(response);}); 
+  //   this.authService.login(this.loginForm.value).subscribe({
+  //     next:(response) => {
+  //       this.matSnackBar.open(response.message, 'Close',{
+  //         duration:5000,
+  //         horizontalPosition: 'center',
+  //       });
+  //       this.router.navigate(['/']);
+  //     },
+  //     error: (error) => {
+  //       this.matSnackBar.open(error.error.message, 'Close', {
+  //         duration:5000,
+  //         horizontalPosition: 'center',
+  //       });
+  //     },
+  //   });
+  // }
 
   ngOnInit():void{
     this.loginForm = this.fb.group({
